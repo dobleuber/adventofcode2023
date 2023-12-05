b863f2b0f189f6fa8a577b20c26c82bbf058b937
Wbert Castro <wbert.castro@genius.com>
2023-12-04 20:01:10 -0500

    Day 4 completed

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,268 @@
+// ignore dead code warnings
+#![allow(dead_code)]
 use regex::Regex;
+use std::char;
 use std::fs::File;
 use std::io::prelude::*;
 
 fn main() {
-    println!("{}", day2().unwrap());
+    println!("{}", day4_part2().unwrap());
 }
 
-fn day2() -> Result<u32, std::io::Error> {
+fn day4_part2() -> Result<u32, std::io::Error> {
+    let contents = open_file("./inputs/4/input.txt")?;
+    let mut cards: Vec<_> = contents
+        .lines()
+        .map(|line| {
+            let line_iter = line.split(':');
+            let cards = line_iter
+                .last()
+                .unwrap()
+                .split('|')
+                .map(|card| {
+                    card.trim()
+                        .split_ascii_whitespace()
+                        .map(|number| number.parse::<u32>().unwrap())
+                        .collect::<Vec<_>>()
+                })
+                .collect::<Vec<_>>();
+            (cards[0].clone(), cards[1].clone(), 1)
+        })
+        .collect();
+
+    let mut index = 0;
+    loop {
+        let card = cards.get(index).unwrap().clone();
+        let wins = card.0.iter().filter(|&n| card.1.contains(n)).count();
+
+        for wins in (index + 1)..=index + wins {
+            if let Some((_, _, ref mut count)) = cards.get_mut(wins) {
+                *count += card.2;
+            }
+        }
+
+        if index == cards.len() - 1 {
+            break;
+        }
+
+        index += 1;
+    }
+
+    let results: Vec<_> = cards.iter().map(|(_, _, count)| *count).collect();
+
+    Ok(results.iter().sum::<u32>())
+}
+
+fn day4_part1() -> Result<u32, std::io::Error> {
+    let contents = open_file("./inputs/4/input.txt")?;
+    // every line looks like this:
+    // Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+    let result: Vec<_> = contents
+        .lines()
+        .map(|line| {
+            let mut line_iter = line.split(':');
+            line_iter.next().unwrap();
+            let cards = line_iter
+                .next()
+                .unwrap()
+                .split('|')
+                .map(|card| {
+                    card.trim()
+                        .split_ascii_whitespace()
+                        .map(|number| number.parse::<u32>().unwrap())
+                        .collect::<Vec<_>>()
+                })
+                .collect::<Vec<_>>();
+            (cards[0].clone(), cards[1].clone())
+        })
+        .map(|(wining_numbers, game_numbers)| {
+            let total_winning_numbers: Vec<u32> = wining_numbers
+                .iter()
+                .filter(|&wn| game_numbers.contains(wn))
+                .cloned()
+                .collect();
+            if total_winning_numbers.is_empty() {
+                0
+            } else {
+                1 << total_winning_numbers.len().checked_sub(1).unwrap()
+            }
+        })
+        .inspect(|line| {
+            dbg!(line);
+        })
+        .collect();
+
+    Ok(result.iter().sum::<u32>())
+}
+
+fn day3_part2() -> Result<u32, std::io::Error> {
+    let contents = open_file("./inputs/3/input.txt")?;
+
+    // convert to matrix
+    let schematic_matrix: Vec<_> = contents
+        .lines()
+        .map(|line| line.chars().collect::<Vec<_>>())
+        .collect();
+
+    let mut schematic_numbers: Vec<u32> = vec![];
+    let mut i = 0;
+
+    while i < schematic_matrix.len() {
+        let line = schematic_matrix.get(i).unwrap();
+        let mut j = 0;
+        while j < line.len() {
+            let char = line.get(j).unwrap();
+            if is_gear(*char) {
+                if let Some(number) = is_valid_gear(&schematic_matrix, i, j) {
+                    schematic_numbers.push(number);
+                }
+            }
+            j += 1;
+        }
+        i += 1;
+    }
+
+    println!("{:?}", schematic_numbers);
+
+    Ok(schematic_numbers.iter().sum::<u32>())
+}
+
+fn is_gear(char: char) -> bool {
+    char == '*'
+}
+
+fn is_valid_gear(schematic_matrix: &Vec<Vec<char>>, i: usize, j: usize) -> Option<u32> {
+    let lower_i = i.saturating_sub(1);
+    let upper_i = schematic_matrix.len().saturating_sub(1).min(i + 1);
+    let lower_j = j.saturating_sub(1);
+    let upper_j = schematic_matrix
+        .get(i)
+        .unwrap()
+        .len()
+        .saturating_sub(1)
+        .min(j + 1);
+    let mut gear_numbers: Vec<u32> = vec![];
+    for i in lower_i..=upper_i {
+        let mut j = lower_j;
+        while j <= upper_j {
+            let char = schematic_matrix.get(i).unwrap().get(j).unwrap();
+            if char.is_ascii_digit() {
+                let whole_number = find_whole_number(schematic_matrix, i, &mut j);
+                gear_numbers.push(whole_number);
+            }
+            j += 1;
+        }
+    }
+
+    print!("{:?} ", gear_numbers);
+
+    if gear_numbers.len() == 2 {
+        Some(gear_numbers.iter().product())
+    } else {
+        None
+    }
+}
+
+fn _day3() -> Result<u32, std::io::Error> {
+    let contents = open_file("./inputs/3/input.txt")?;
+
+    // convert to matrix
+    let schematic_matrix: Vec<_> = contents
+        .lines()
+        .map(|line| line.chars().collect::<Vec<_>>())
+        .collect();
+
+    let mut schematic_numbers: Vec<u32> = vec![];
+    let mut i = 0;
+
+    while i < schematic_matrix.len() {
+        let line = schematic_matrix.get(i).unwrap();
+        let mut j = 0;
+        while j < line.len() {
+            let char = line.get(j).unwrap();
+            if char.is_ascii_digit() && number_is_valid(&schematic_matrix, i, j) {
+                let whole_number = find_whole_number(&schematic_matrix, i, &mut j);
+                schematic_numbers.push(whole_number);
+            }
+            j += 1;
+        }
+        i += 1;
+    }
+
+    println!("{:?}", schematic_numbers);
+
+    Ok(schematic_numbers.iter().sum::<u32>())
+}
+
+fn number_is_valid(schematic_matrix: &Vec<Vec<char>>, i: usize, j: usize) -> bool {
+    let lower_i = i.saturating_sub(1);
+    let upper_i = schematic_matrix.len().saturating_sub(1).min(i + 1);
+    let lower_j = j.saturating_sub(1);
+    let upper_j = schematic_matrix
+        .get(i)
+        .unwrap()
+        .len()
+        .saturating_sub(1)
+        .min(j + 1);
+    for i in lower_i..=upper_i {
+        for j in lower_j..=upper_j {
+            let char = schematic_matrix.get(i).unwrap().get(j).unwrap();
+            if is_symbol(*char) {
+                return true;
+            }
+        }
+    }
+
+    false
+}
+
+fn is_symbol(char: char) -> bool {
+    char != '.' && !char.is_alphanumeric() && !char.is_ascii_whitespace()
+}
+
+fn find_whole_number(schematic_matrix: &Vec<Vec<char>>, i: usize, j: &mut usize) -> u32 {
+    let mut number_start_index = *j;
+
+    let line = schematic_matrix.get(i).unwrap();
+
+    loop {
+        let temp_index = number_start_index.saturating_sub(1);
+        let char = line.get(temp_index).unwrap();
+        if char.is_ascii_digit() {
+            number_start_index = temp_index;
+        } else {
+            break;
+        }
+
+        if temp_index == 0 {
+            break;
+        }
+    }
+
+    loop {
+        let temp_index = *j + 1;
+        let char = line.get(temp_index).unwrap();
+        if char.is_ascii_digit() {
+            *j = temp_index;
+        } else {
+            break;
+        }
+
+        if temp_index == line.len() - 1 {
+            break;
+        }
+    }
+
+    let number: String = line
+        .iter()
+        .skip(number_start_index)
+        .take(*j - number_start_index + 1)
+        .collect();
+
+    number.parse::<u32>().unwrap()
+}
+
+fn _day2() -> Result<u32, std::io::Error> {
     let contents = open_file("./inputs/2/input.txt")?;
 
     // Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
